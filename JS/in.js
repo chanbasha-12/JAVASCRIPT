f4d29332af13fe54e8d131d0a08b00ca1b3e1ebd
chanbasha-12 <skchanbasha1111@gmail.com>
2021-02-05 11:26:26 +0530

    added git commit

---
+++ JS/in.js
@@ -0,0 +1,13 @@
+  var container=document.querySelector("#root");
+  var card=document.createElement("div");
+ card.setAttribute("id0","card");
+  container.appendChild(card);
+ var image=document.createElement("img");
+ image.src="picture/img.png";
+  image.alt="male";
+ image.style.width="20%";
+  card.appendChild(image);
+  document.write("<h1>SK CHANBASHA</h1>");
+  document.write("<h2>SKCHANBASHA1111@gmail.com</h2>");
+  document.write("<h2>computer science engineering</h2>");
+ 
